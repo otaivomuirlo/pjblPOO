f97d4aa4f915c97d136530e7a591953ddd535f75
otaivomuirlo <132911950+otaivomuirlo@users.noreply.github.com>
2023-10-20 05:27:34 -0300

    Delete PJBL directory

--- PJBL/Eletrodomesticos.java
+++
@@ -1,6 +0,0 @@
-public class Eletrodomesticos extends Produto {
-    public Eletrodomesticos(String nome, double valor){
-        super(nome, valor);
-
-    }
-}
--- PJBL/PetShop.java
+++
@@ -1,8 +0,0 @@
-public class PetShop extends Produto{
-    public PetShop(String nome, double valor){
-        super(nome, valor);
-
-    }
-
-    
-}
