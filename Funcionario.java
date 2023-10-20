0a138231df7b0e65774320dbebc7731c71c19671
otaivomuirlo <132911950+otaivomuirlo@users.noreply.github.com>
2023-10-20 05:29:22 -0300

    Delete Funcionario.java

--- Funcionario.java
+++
@@ -1,9 +0,0 @@
-public class Funcionario extends Pessoas {
-    
-    public Funcionario(String nome, int CPF, int idade){
-        super(nome, CPF, idade);
-    }
-    public void reestocar(){
-        
-    }
-}
