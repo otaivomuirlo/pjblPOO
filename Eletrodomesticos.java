0e7bf7850ea780f955b7cdc1ccb829b04ec0ceaa
otaivomuirlo <132911950+otaivomuirlo@users.noreply.github.com>
2023-10-20 05:28:55 -0300

    Delete Eletrodomesticos.java

--- Eletrodomesticos.java
+++
@@ -1,6 +0,0 @@
-public class Eletrodomesticos extends Produto {
-    public Eletrodomesticos(String nome, double valor , int estoque){
-        super(nome, estoque, valor, estoque, estoque);
-
-    }
-}
