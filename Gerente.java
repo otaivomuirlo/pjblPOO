60793fb660713e88eab71c914d5e101dbb947354
otaivomuirlo <132911950+otaivomuirlo@users.noreply.github.com>
2023-10-20 05:31:40 -0300

    Delete Gerente.java

--- Gerente.java
+++
@@ -1,8 +0,0 @@
-public class Gerente extends Funcionario {
-    public Gerente(String nome, int CPF, int idade){
-        super(nome, CPF, idade);
-    }
-    void mostrarProdutos(Papelaria p){
-        p.mostrarProdutos();
-    }
-}
